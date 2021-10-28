a9ec31490ccce6df8f3b2c4c08839f4242aa1a54
Manuel Vogel <mavogel@posteo.de>
2021-10-28 10:51:25 +0300

    chore(ci): updating to latest script fix

--- install-docker.sh
+++ install-docker.sh
@@ -2,6 +2,11 @@
 
 set -ex
 
+# from:
+# https://github.com/docker/roadmap/issues/80#issuecomment-633613240
+# and update
+# https://github.com/docker/for-mac/issues/2359#issuecomment-853420567
+
 # refs:
 # https://github.com/MicrosoftDocs/vsts-docs/issues/3784
 # https://forums.docker.com/t/docker-for-mac-unattended-installation/27112
@@ -12,9 +17,47 @@ echo "allow the app to run without confirmation"
 xattr -d -r com.apple.quarantine /Applications/Docker.app
 
 echo "preemptively do docker.app's setup to avoid any gui prompts"
-sudo "$(which cp)" /Applications/Docker.app/Contents/Library/LaunchServices/com.docker.vmnetd /Library/PrivilegedHelperTools
-# exists already in destination 
-#sudo "$(which cp)" /Applications/Docker.app/Contents/Resources/com.docker.vmnetd.plist /Library/LaunchDaemons/
-sudo "$(which chmod)" 544 /Library/PrivilegedHelperTools/com.docker.vmnetd
-sudo "$(which chmod)" 644 /Library/LaunchDaemons/com.docker.vmnetd.plist
-sudo "$(which launchctl)" load /Library/LaunchDaemons/com.docker.vmnetd.plist
+sudo cp /Applications/Docker.app/Contents/Library/LaunchServices/com.docker.vmnetd /Library/PrivilegedHelperTools
+
+# get vmnetd version number
+defaults read /Applications/Docker.app/Contents/Info.plist VmnetdVersion
+
+# the plist we need used to be in /Applications/Docker.app/Contents/Resources, but
+# is now dynamically generated. So we dynamically generate our own
+sudo tee "/Library/LaunchDaemons/com.docker.vmnetd.plist" > /dev/null <<'EOF'
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+	<key>Label</key>
+	<string>com.docker.vmnetd</string>
+	<key>Program</key>
+	<string>/Library/PrivilegedHelperTools/com.docker.vmnetd</string>
+	<key>ProgramArguments</key>
+	<array>
+		<string>/Library/PrivilegedHelperTools/com.docker.vmnetd</string>
+	</array>
+	<key>RunAtLoad</key>
+	<true/>
+	<key>Sockets</key>
+	<dict>
+		<key>Listener</key>
+		<dict>
+			<key>SockPathMode</key>
+			<integer>438</integer>
+			<key>SockPathName</key>
+			<string>/var/run/com.docker.vmnetd.sock</string>
+		</dict>
+	</dict>
+	<key>Version</key>
+	<string>59</string>
+</dict>
+</plist>
+
+EOF
+
+sudo chmod 544 /Library/PrivilegedHelperTools/com.docker.vmnetd
+sudo chmod 644 /Library/LaunchDaemons/com.docker.vmnetd.plist
+sudo launchctl load /Library/LaunchDaemons/com.docker.vmnetd.plist
+
+sleep 5
